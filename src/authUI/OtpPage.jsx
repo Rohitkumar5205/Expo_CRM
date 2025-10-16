8619bcd6b0b83c6648ad02ce836b6c042e92c89a
Rohitkumar5205 <rohit@arogyamantra.com>
2025-10-16 15:19:18 +0530

    update otp page
    update otp page

--- src/authUI/OtpPage.jsx
+++ src/authUI/OtpPage.jsx
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
-import img from '../assets/images/login-bg.jpg';
-import img2 from '../assets/images/logo.png';
+import React, { useState } from "react";
+import img from "../assets/images/login-bg.jpg";
+import img2 from "../assets/images/logo.png";
 import { FaRegCopyright } from "react-icons/fa6";
-import { showInfo, showSuccess } from '../utils/toastMessage';
-import { useNavigate } from 'react-router-dom';
+import { showInfo, showSuccess } from "../utils/toastMessage";
+import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import img from "../assets/images/login-bg.jpg";
 import img2 from "../assets/images/logo.png";
 
 const OtpPage = () => {
-  const [otp, setOtp] = useState('');
-  // page navigation on success of otp 
-const navigate=useNavigate()
+  const [otp, setOtp] = useState("");
+  // page navigation on success of otp
+  const navigate = useNavigate();
 
   // 🔹 Handle form submit
   const handleSubmit = (e) => {
     e.preventDefault();
-     showSuccess("OTP is verified successfully")
-    navigate('/dashboard')
-    console.log('Verifying OTP:', otp);
+    showSuccess("OTP is verified successfully");
+    navigate("/dashboard");
+    console.log("Verifying OTP:", otp);
   };
 
-  // input value logic 
-const handleChange = (e) => {
-  const value = e.target.value.replace(/\D/g, '').slice(0, 4);
-  setOtp(value);
-};
-
-
+  // input value logic
+  const handleChange = (e) => {
+    const value = e.target.value.replace(/\D/g, "").slice(0, 4);
+    setOtp(value);
+  };
 
   // 🔹 Handle resend
   const handleResend = () => {
-    console.log('Resending OTP');
-    showInfo("Your four-digit OTP has been sent to your WhatsApp")
+    console.log("Resending OTP");
+    showInfo("Your four-digit OTP has been sent to your WhatsApp");
   };
 
   return (
     <div
       style={{
-        width: '100%',
-        minHeight: '100vh',
+        width: "100%",
+        minHeight: "100vh",
         backgroundImage: `url(${img})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        flexDirection: 'column',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <div className="flex justify-center flex-1 items-center px-4">
@@ -75,7 +73,7 @@ const handleChange = (e) => {
               className="w-full pl-3 pr-3 py-1 md:py-1.5 mb-3 md:mb-4 text-sm bg-white border border-gray-300 focus:outline-none focus:border-blue-500 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
               type="number"
               name="otp"
-              placeholder="Enter your OTP number" 
+              placeholder="Enter your OTP number"
               value={otp}
               onChange={handleChange}
               required
@@ -84,12 +82,8 @@ const handleChange = (e) => {
             <button
               className="w-full mb-1 md:mb-1 py-2 md:py-1.5 bg-[#337ab7] text-white text-sm md:text-sm font-medium"
               type="submit"
-              onMouseOver={(e) =>
-                (e.target.style.backgroundColor = '#164771')
-              }
-              onMouseOut={(e) =>
-                (e.target.style.backgroundColor = '#337ab7')
-              }
+              onMouseOver={(e) => (e.target.style.backgroundColor = "#164771")}
+              onMouseOut={(e) => (e.target.style.backgroundColor = "#337ab7")}
             >
               Verify
             </button>
@@ -98,12 +92,8 @@ const handleChange = (e) => {
               className="w-full py-2 md:py-1.5 bg-[#337ab7] text-white text-sm md:text-sm font-medium"
               type="button"
               onClick={handleResend}
-              onMouseOver={(e) =>
-                (e.target.style.backgroundColor = '#164771')
-              }
-              onMouseOut={(e) =>
-                (e.target.style.backgroundColor = '#337ab7')
-              }
+              onMouseOver={(e) => (e.target.style.backgroundColor = "#164771")}
+              onMouseOut={(e) => (e.target.style.backgroundColor = "#337ab7")}
             >
               Resend
             </button>
@@ -115,13 +105,17 @@ const handleChange = (e) => {
       <footer className="py-2 md:py-2">
         <ul className="flex justify-center gap-2 text-sm sm:text-sm list-none">
           <li>
-            <a href="https://namogange.org/" className="hover:text-zinc-700 text-blue-600 hover:underline ">
-              Namo Gange Trust 
+            <a
+              href="https://namogange.org/"
+              className="hover:text-zinc-700 text-blue-600 hover:underline "
+            >
+              Namo Gange Trust
             </a>
           </li>
           <li className="text-gray-700">|</li>
           <li className="text-black flex">
-            <FaRegCopyright className="pt-1" />2017
+            <FaRegCopyright className="pt-1" />
+            2017
           </li>
         </ul>
       </footer>
